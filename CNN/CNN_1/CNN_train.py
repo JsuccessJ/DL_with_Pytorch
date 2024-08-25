990559f9632206792f6fb485de302261f6b0f8f4
JS LAB <132157712+JsuccessJ@users.noreply.github.com>
2024-08-25 22:30:41 +0900

    Update CNN_train.py

--- CNN/CNN_1/CNN_train.py
+++ CNN/CNN_1/CNN_train.py
@@ -62,7 +62,7 @@ for epoch in range(3):
     epoch_loss = running_loss / len(dataloader_train)
     print(f"Epoch {epoch+1}, Loss: {epoch_loss:.4f}")
 
-
+# multiclass model evaluation
 # Define metrics
 metric_precision = Precision(task="multiclass", num_classes=7, average="micro")
 metric_recall = Recall(task="multiclass", num_classes=7, average="micro")
@@ -79,3 +79,25 @@ precision = metric_precision.compute()
 recall = metric_recall.compute()
 print(f"Precision: {precision}")
 print(f"Recall: {recall}")
+
+# per class
+# Define precision metric
+metric_precision = Precision(
+    task="multiclass", num_classes=7, average=None
+)
+
+net.eval()
+with torch.no_grad():
+    for images, labels in dataloader_test:
+        outputs = net(images)
+        _, preds = torch.max(outputs, 1)
+        metric_precision(preds, labels)
+precision = metric_precision.compute()
+
+# Get precision per class
+precision_per_class = {
+    k: precision[v].item()
+    for k, v 
+    in dataset_test.class_to_idx.items()
+}
+print(precision_per_class)
